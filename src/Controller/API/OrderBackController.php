bd030232cbf723a7e81f022c2dc50790361dc05f
Boris Bobol <bobol.boris@gmail.com>
2020-06-11 06:32:09 +0300

    11.06.2020 - 06:32

--- src/Controller/API/OrderBackController.php
+++ src/Controller/API/OrderBackController.php
@@ -2,17 +2,43 @@
 
 namespace App\Controller\API;
 
-use App\Controller\ApiController;
+use App\Controller\Controller;
+use App\Service\API\OrderService;
+use Exception;
 use Symfony\Component\HttpFoundation\Response;
 
-class OrderBackController extends ApiController
+class OrderBackController extends Controller
 {
+    /** @var OrderService $orderService */
+    protected $orderService;
+
+    /**
+     * OrderBackController constructor.
+     * @param OrderService $orderService
+     */
+    public function __construct(OrderService $orderService)
+    {
+        $this->orderService = $orderService;
+    }
+
     /**
      * @param int $id
      * @return Response
      */
     public function orderInformationAction(int $id): Response
     {
+        try {
+            $data = [
+                'ok' => true,
+                'data' => $this->orderService->getOrderInformation($id)
+            ];
+        } catch (Exception $e) {
+            $data = [
+                'ok' => false,
+                'errors' => [$e->getMessage()]
+            ];
+        }
 
+        return $this->json($data);
     }
 }
--- src/Service/API/OrderService.php
+++ src/Service/API/OrderService.php
@@ -2,9 +2,121 @@
 
 namespace App\Service\API;
 
+use App\Entity\Back\OrderGamePost as OrderBack;
+use App\Exception\OrderBackNotFoundException;
+use App\Repository\Back\OrderGamePostRepository as OrderBackRepository;
+use App\Repository\Back\ShippingMethodRepository as ShippingMethodBackRepository;
 use App\Service\Service;
+use App\Repository\Back\PaymentTypeRepository as PaymentMethodBackRepository;
 
 class OrderService extends Service
 {
+    /** @var OrderBackRepository $orderBackRepository */
+    protected $orderBackRepository;
 
+    /** @var ShippingMethodBackRepository $shippingMethodBackRepository */
+    protected $shippingMethodBackRepository;
+
+    /** @var PaymentMethodBackRepository $paymentMethodBackRepository */
+    protected $paymentMethodBackRepository;
+
+    /**
+     * OrderService constructor.
+     * @param OrderBackRepository $orderBackRepository
+     * @param ShippingMethodBackRepository $shippingMethodBackRepository
+     * @param PaymentMethodBackRepository $paymentMethodBackRepository
+     */
+    public function __construct(
+        OrderBackRepository $orderBackRepository,
+        ShippingMethodBackRepository $shippingMethodBackRepository,
+        PaymentMethodBackRepository $paymentMethodBackRepository
+    )
+    {
+        $this->orderBackRepository = $orderBackRepository;
+        $this->shippingMethodBackRepository = $shippingMethodBackRepository;
+        $this->paymentMethodBackRepository = $paymentMethodBackRepository;
+    }
+
+    /**
+     * @param int $id
+     * @return array
+     * @throws OrderBackNotFoundException
+     */
+    public function getOrderInformation(int $id): array
+    {
+        $ordersBack = $this->orderBackRepository->findByOrderNum($id);
+        if (0 === count($ordersBack)) {
+            throw new OrderBackNotFoundException('Order not found');
+        }
+
+        $orderBack = $ordersBack[0];
+
+        $data = [
+            'type' => $orderBack->getType(),
+            'FIO' => $orderBack->getFio(),
+            'phone' => $orderBack->getPhone(),
+            'mail' => $orderBack->getMail(),
+            'region' => $orderBack->getRegion(),
+            'city' => $orderBack->getCity(),
+            'street' => $orderBack->getStreet(),
+            'house' => $orderBack->getHouse(),
+            'warehouse' => $orderBack->getWarehouse(),
+            'delivery' => $this->getDeliveryName($orderBack->getDelivery()),
+            'payment' => $this->getPaymentName($orderBack->getPayment()),
+            'comment' => $orderBack->getComments(),
+            'products' => $this->getProducts($ordersBack),
+        ];
+
+        return $data;
+    }
+
+    /**
+     * @param OrderBack[] $orders
+     * @return array
+     */
+    protected function getProducts(array $orders): array
+    {
+        $data = [];
+
+        foreach ($orders as $order) {
+            $data[] = [
+                'name' => $order->getProductName(),
+                'price' => $order->getPrice(),
+                'amount' => $order->getAmount(),
+                'currency_name' => $order->getCurrencyName(),
+                'rate' => $order->getCurrencyValue(),
+            ];
+        }
+
+
+        return $data;
+    }
+
+    /**
+     * @param int $id
+     * @return string
+     */
+    protected function getDeliveryName(int $id): string
+    {
+        $shippingMethod = $this->shippingMethodBackRepository->find($id);
+        if (null === $shippingMethod) {
+            return $shippingMethod->getName();
+        }
+
+        return 'Неизвестно';
+    }
+
+    /**
+     * @param int $id
+     * @return string
+     */
+    protected function getPaymentName(int $id): string
+    {
+        $paymentMethod = $this->paymentMethodBackRepository->find($id);
+        if (null === $paymentMethod) {
+            return $paymentMethod->getName();
+        }
+
+        return 'Неизвестно';
+    }
 }
